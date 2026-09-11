1cba4e14cc98b5b93252176a4ea9f169d72b5f13
ShamsulHaque12 <mdshamsulhaquesujon@gmail.com>
2026-09-11 12:14:33 +0600

    feat: add Go exercises for implementing interfaces and type assertions

---
+++ Day3/exercise1.go
@@ -0,0 +1,54 @@
+package main
+
+/*
+import "fmt"
+
+type Dog struct {
+	Name string
+}
+
+type Cat struct {
+	Name string
+}
+
+type Animal interface {
+	Speak()
+	Move()
+}
+
+func (d Dog) Speak() {
+	fmt.Printf("%s says: Woof! Woof!\n", d.Name)
+}
+
+func (d Dog) Move() {
+	fmt.Printf("%s is running happily.\n", d.Name)
+}
+
+func (c Cat) Speak() {
+	fmt.Printf("%s says: Meow! Meow!\n", c.Name)
+}
+
+func (c Cat) Move() {
+	fmt.Printf("%s is walking softly.\n", c.Name)
+}
+
+func showAnimal(a Animal) {
+	a.Speak()
+	a.Move()
+	fmt.Println("--------------------")
+}
+
+func main() {
+	fmt.Println("==== Interface =====")
+
+	dog := Dog{Name: "Buddy"}
+	cat := Cat{Name: "Kitty"}
+
+	fmt.Println("Dog Details:")
+	showAnimal(dog)
+
+	fmt.Println("Cat Details:")
+	showAnimal(cat)
+}
+
+*/
---
+++ Day3/exercise2.go
@@ -0,0 +1,76 @@
+package main
+
+import "fmt"
+
+type Dog struct {
+	Name string
+}
+
+type Cat struct {
+	Name string
+}
+
+type Animal interface {
+	Speak()
+	Move()
+}
+
+func (d Dog) Speak() {
+	fmt.Printf("%s says: Woof! Woof!\n", d.Name)
+}
+
+func (d Dog) Move() {
+	fmt.Printf("%s is running happily.\n", d.Name)
+}
+
+func (c Cat) Speak() {
+	fmt.Printf("%s says: Meow! Meow!\n", c.Name)
+}
+
+func (c Cat) Move() {
+	fmt.Printf("%s is walking softly.\n", c.Name)
+}
+
+// Function using single Type Assertion with comma-ok idiom
+func checkTypeWithAssertion(a Animal) {
+	dog, ok := a.(Dog)
+	if ok {
+		fmt.Printf("[Type Assertion] Found a Dog with name: %s\n", dog.Name)
+		return
+	}
+
+	cat, ok := a.(Cat)
+	if ok {
+		fmt.Printf("[Type Assertion] Found a Cat with name: %s\n", cat.Name)
+		return
+	}
+
+	fmt.Println("[Type Assertion] Unknown animal type")
+}
+
+// Function using Type Switch
+func checkTypeWithSwitch(a Animal) {
+	switch v := a.(type) {
+	case Dog:
+		fmt.Printf("[Type Switch] It's a Dog! Name: %s\n", v.Name)
+	case Cat:
+		fmt.Printf("[Type Switch] It's a Cat! Name: %s\n", v.Name)
+	default:
+		fmt.Println("[Type Switch] Unknown type")
+	}
+}
+
+func main() {
+	fmt.Println("==== Type Assertion ====")
+
+	var a1 Animal = Dog{Name: "Buddy"}
+	var a2 Animal = Cat{Name: "Kitty"}
+
+	fmt.Println("\n--- Testing Animal 1 (Dog) ---")
+	checkTypeWithAssertion(a1)
+	checkTypeWithSwitch(a1)
+
+	fmt.Println("\n--- Testing Animal 2 (Cat) ---")
+	checkTypeWithAssertion(a2)
+	checkTypeWithSwitch(a2)
+}
